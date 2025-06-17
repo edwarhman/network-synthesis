4642f73d2ccae1092c2c2bda040da1f491ee4c65
Emerson Warhman <edwarhman@gmail.com>
2025-06-16 23:28:14 -0400

    crear clase abstracta para clase RedElectrica

--- src/red_electrica.py
+++ src/red_electrica.py
@@ -1,27 +1,30 @@
+from abc import ABC, abstractmethod
+
 from sympy import roots, sympify
 from sympy.abc import s
 
 
-class RedElectrica:
-    def __init__(self, expresion):
-        self._expresion = sympify(expresion)
+class RedElectricaBase(ABC):
+    """
+    Clase base para representar una red eléctrica.
+    Esta clase define la estructura básica de una red eléctrica y sus operaciones.
+    """
 
+    @abstractmethod
     def ceros_y_polos(self):
         """
-        Calcula los ceros y polos de una expresión de transferencia.
-        :param expresion: Expresión de transferencia
-        :return: Tupla con los ceros y polos
+        Método abstracto para calcular los ceros y polos de la red.
+        Debe ser implementado por las subclases.
         """
-        num, den = self._expresion.as_numer_denom()
-        ceros = list(roots(num, s).keys())
-        polos = list(roots(den, s).keys())
-        return ceros, polos
+        pass
 
+    @abstractmethod
     def gain(self):
-        raise NotImplementedError("This method should be implemented by subclasses.")
-
-    def expresion(self):
-        return self._expresion
+        """
+        Método abstracto para calcular la ganancia de la red.
+        Debe ser implementado por las subclases.
+        """
+        pass
 
     @staticmethod
     def paralelo(red1: "RedElectrica", red2: "RedElectrica") -> "RedElectrica":
@@ -46,3 +49,25 @@ class RedElectrica:
         for red in redes:
             expresion_total += red.expresion()
         return RedElectrica(expresion_total)
+
+
+class RedElectrica(RedElectricaBase):
+    def __init__(self, expresion):
+        self._expresion = sympify(expresion)
+
+    def ceros_y_polos(self):
+        """
+        Calcula los ceros y polos de una expresión de transferencia.
+        :param expresion: Expresión de transferencia
+        :return: Tupla con los ceros y polos
+        """
+        num, den = self.expresion().as_numer_denom()
+        ceros = list(roots(num, s).keys())
+        polos = list(roots(den, s).keys())
+        return ceros, polos
+
+    def gain(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
+
+    def expresion(self):
+        return self._expresion
