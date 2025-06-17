cc447133e47f3c95b57c7b07e92ab1fefa545ec6
Emerson Warhman <edwarhman@gmail.com>
2025-06-16 23:36:10 -0400

    feat: agregar clase compuesta RedSerie

---
+++ src/red_serie.py
@@ -0,0 +1,29 @@
+from src.red_electrica import RedElectrica, RedElectricaBase
+
+
+class RedSerie(RedElectricaBase):
+    def __init__(self, *redes):
+        redesElectricas = []
+        for red in redes:
+            if isinstance(red, RedElectrica):
+                redesElectricas.append(red)
+            else:
+                redesElectricas.append(RedElectrica(red))
+        self.redes = redesElectricas
+
+    def ceros_y_polos(self):
+        """
+        Calcula los ceros y polos de una red en serie.
+        :return: Tupla con los ceros y polos
+        """
+        return RedElectricaBase.serie(self.redes).ceros_y_polos()
+
+    def expresion(self):
+        """
+        Devuelve la expresión de la red en serie.
+        :return: Expresión de la red en serie
+        """
+        return RedElectricaBase.serie(self.redes).expresion()
+
+    def gain(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
---
+++ tests/test_red_serie.py
@@ -0,0 +1,50 @@
+import pytest
+
+from src.red_serie import (
+    RedSerie,
+)
+from tests.utils import comparar_expresiones
+
+
+class TestRedSerie:
+    @pytest.mark.parametrize(
+        "expresion, resultado_esperado",
+        [
+            (["s**2", "2*s", "1"], "s**2 + 2*s + 1"),
+            (["s**3", "3*s**2", "3*s", "1"], "s**3 + 3*s**2 + 3*s + 1"),
+            (
+                ["s**4", "4*s**3", "6*s**2", "4*s", "1"],
+                "s**4 + 4*s**3 + 6*s**2 + 4*s + 1",
+            ),
+            (["s", "1", "1/s"], "s + 1 + 1/s"),
+        ],
+    )
+    def test_obtener_expresion(self, expresion, resultado_esperado):
+        # Arrange
+        red = RedSerie(*expresion)
+        # Act
+        expresion_resultado = red.expresion()
+        # Assert
+        assert comparar_expresiones(expresion_resultado, resultado_esperado)
+
+    @pytest.mark.parametrize(
+        "expresion, ceros_esperados, polos_esperados",
+        [
+            (["s**2", "2*s", "1"], [-1], []),
+        ],
+    )
+    def test_ceros_y_polos(self, expresion, ceros_esperados, polos_esperados):
+        # Arrange
+        red = RedSerie(*expresion)
+        # Act
+        ceros, polos = red.ceros_y_polos()
+        # Assert
+        assert ceros == ceros_esperados
+        assert polos == polos_esperados
+
+    def test_metodos_no_implementados(self):
+        # Test that methods raise NotImplementedError
+        red = RedSerie(["s**2 + 2*s + 1"])
+
+        with pytest.raises(NotImplementedError):
+            red.gain()
